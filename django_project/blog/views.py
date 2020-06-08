d48f95bb05616fa4b498b512e8bc4f4b27cbe0df
harsh52 <hkumar8897@gmail.com>
2020-06-08 22:05:31 +0530

    Added bootstrap

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -2,8 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+posts = [
+	{
+		'author':'coreyms',
+		'title':'Blog',
+		'content':'First post',
+		'data_posted':'August 27,2019'
+	},
+	{
+		'author':'Jane Doe',
+		'title':'Blog 2',
+		'content':'Second post',
+		'data_posted':'June 27,2019'
+	}
+]
+
 def home(request):
-	return  HttpResponse('<h1>Blog Home</h1>')
+	context = {
+		'posts':posts
+	}
+	return  render(request,'blog/home.html',context)
 
 def about(request):
-	return HttpResponse('<h1>Blog About </h1>')
+	return  render(request,'blog/About.html',{'title' : 'About'})
